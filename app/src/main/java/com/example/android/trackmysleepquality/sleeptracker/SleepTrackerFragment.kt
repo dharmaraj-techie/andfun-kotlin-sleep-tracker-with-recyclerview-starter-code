191442003eac840edf4879f73526a89a1ccd8681
Dharmaraj <dharmaraj.techie@gmail.com>
2020-05-22 23:23:00 +0530

    add recyclerView Adapter

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -97,6 +97,18 @@ class SleepTrackerFragment : Fragment() {
             }
         })
 
+
+        val adapter = SleepNightAdapter()
+
+        binding.sleepList.adapter = adapter
+
+        sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.data = it
+            }
+        })
+
+
         return binding.root
     }
 }
